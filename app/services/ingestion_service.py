791c9b2d1b99ddb93a4ef808fc2a7f76a28389b0
mroshan454 <roshanpkr2000@gmail.com>
2026-05-19 19:57:50 +1000

    Switching to OpenAI embeddings, remove PyTorch/HF to reduce image size

--- app/services/ingestion_service.py
+++ app/services/ingestion_service.py
@@ -1,12 +1,12 @@
 from langchain_community.document_loaders import PyPDFLoader 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma 
 import hashlib 
 
 #Initialise the embedding model 
-_embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-V2"
+_embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-small"
 )
 
 def load_pdf(file_path: str):
